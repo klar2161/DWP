0fbf9f582a9b69e0f7b3f83115db2ba45cea995c
gyetvanakos <gyetvanakos@gmail.com>
2021-11-09 12:23:33 +0100

    feedfix

--- includes/feed.inc.php
+++ includes/feed.inc.php
@@ -10,17 +10,19 @@ $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 include('session.php');
 
 
-
 if (isset($_POST['post'])){
 
     header('feed.php');
 
     $post = $_POST["post"];
 
-if(isset($_GET['id'])){
-    $query = "DELETE FROM `posts` WHERE `postID`=". $_GET['id'];
-    mysqli_query($conn, $query);
+
     
 
 mysqli_query($conn,"insert into posts (post,userID) values ('$post','$userid') ")or die(mysqli_error());
-};
+}
+
+if(isset($_GET['id'])){
+    $query = "DELETE FROM `posts` WHERE `postID`=". $_GET['id'];
+    mysqli_query($conn, $query);
+}
