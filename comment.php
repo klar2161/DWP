9c9014c80e39250749a8ff22ad7e49017e7f65fe
gyetvanakos <gyetvanakos@gmail.com>
2021-11-22 10:53:36 +0100

    comments (not working)

--- comment.php
+++ comment.php
@@ -2,13 +2,37 @@
 include_once("dbh.inc.php");
 include('session.php');
 
-if (isset($_POST['comment'])){
-
-    header("location: ../feed.php");
+/*if (isset($_POST['content'])){
 
     $content = $_POST["content"];
-    
 
-    mysqli_query($conn,"INSERT INTO comments (content) VALUES ('ez egy komment te kurva') ")or die(mysqli_error());
+mysqli_query($conn,"insert into comments (content,userID) values ('$content','$userid') ")or die(mysqli_error());*/
+
+if (isset($_POST['content'])) {
+
+    $content = $_POST["content"];
+
+    createComment($conn, $commentID, $content, $userid);
 
 }
+else {
+    header("location: ../feed.php");
+    exit();
+}
+
+
+function createComment($conn, $commentID, $content, $userid) {
+
+    header("location: ../feed.php");
+    
+    $sql = "INSERT INTO comments (commentID, content, userID) VALUES (NULL, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../feed.php");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "isi", $commentID, $content, $userid);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
+
--- feed.php
+++ feed.php
@@ -8,15 +8,18 @@ include_once 'header.php';
 <form action="includes/feed.inc.php" method="post" class="postbox">
         <textarea type="text" name="post"  rows="7" cols="64" style="" placeholder="What's in your head?" required></textarea>
         <br>
-        <button type="submit" name="submit" class="postbox">Post</button>
+        <button type="submit" name="submit">Post</button>
 </form>
 </div>
 
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" name="submit">
+</form>
+
 <?php
 
-require_once("includes/dbh.inc.php");
-
-$conn= conn();
+include_once("includes/dbh.inc.php");
 
 $query = "SELECT posts.postID, users.usersuid, posts.post
 FROM posts
@@ -25,25 +28,18 @@ $result = mysqli_query($conn, $query) or die("its ded");
 while($row = mysqli_fetch_array($result)){
     echo  
     "<h1>".$row["usersuid"]."</h1>"."<a href='deletepost.php?id=".$row['postID']."'>  X</a><br>".
-    "<h2>".$row["post"]."</h2>";   
+    "<h2>".$row["post"]."</h2>"; 
 }
 ?>
 
-<form action="includes/comment.php" method="post">
-	<textarea type="text" name="comment" rows="2" cols="44" style="" placeholder="Comment" required></textarea><br>
-	<button type="submit" name="comment" class="comment">Comment</button>
+<form action="includes/comment.php" method="post" class="postbox">
+        <textarea type="text" name="content" id="content" rows="2" cols="64" style="" placeholder="Comment" required></textarea>
+        <br>
+        <button type="submit" name="submit">Comment</button>
 </form>
 
-<?php 
-	/*$query = "SELECT posts.postID, users.usersuid, posts.post
-    FROM posts
-    INNER JOIN users ON posts.userID=users.userID;";
-    $result = mysqli_query($conn, $query) or die("its ded");
-    while($row = mysqli_fetch_array($result)){
-        echo  
-        "<h1>".$row["usersuid"]."</h1>"."<a href='deletepost.php?id=".$row['postID']."'>  X</a><br>".
-        "<h2>".$row["post"]."</h2>";   
-    }	*/
+<?php
+							
 ?>
 
 <section class="index-intro">
