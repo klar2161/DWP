08f6d0ea6a98cad34939a8c9237f233587c52edf
gyetvanakos <gyetvanakos@gmail.com>
2021-11-26 09:27:02 +0100

    comment( almost there )

--- Application/comment.php
+++ Application/comment.php
@@ -6,30 +6,32 @@ if (isset($_POST['content'])) {
 
     $content = $_POST["content"];
     $userid = $_POST["uid"];
+    $postID = $_POST["postID"];
     
-    createComment($conn, $content, $userid);
+    createComment($conn, $content, $userid, $postID);
+    
 
     header("location: ../Presentation/feed.php");
 
 }
 else {
-    header("location: ..../Presentation/feed.php");
+    header("location:   ../Presentation/feed.php");
     exit();
 }
 
 
 
-function createComment($conn, $content, $userid) {
+function createComment($conn, $content, $userid, $postID) {
     
-    $sql = "INSERT INTO `Comments` (`commentID`, `content`, `userID`) VALUES (NULL, ?, ?);";
+    $sql = "INSERT INTO `Comments` (`commentID`, `content`, `userID`, `postID`) VALUES (NULL, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ..../Presentation/feed.php");
+        header("location: ../Presentation/feed.php");
         
         exit();
     }
     var_dump($stmt);
-    mysqli_stmt_bind_param($stmt, "si", $content, $userid);
+    mysqli_stmt_bind_param($stmt, "sii", $content, $userid, $postID);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
 }
--- Presentation/feed.php
+++ Presentation/feed.php
@@ -14,18 +14,23 @@ include_once '../DataAcces/connectDB.php';
 </div>
 
 <?php
-	$query = "SELECT Posts.postID, users.usersuid, Posts.post, Posts.userID
+	$query = "SELECT Posts.postID, users.usersuid, Posts.post, Posts.userID, comments.content
     FROM Posts
-    INNER JOIN users ON Posts.userID=users.userID;";
+    JOIN users ON Posts.userID=users.userID
+    JOIN comments ON Posts.userID=comments.userID";
     $result = mysqli_query($conn, $query) or die("its ded");
     while($row = mysqli_fetch_assoc($result)){
         echo  
-        "<h1>".$row["usersuid"]."</h1>"."<a href='../Application/deletepost.php?id=".$row['postID']."'>  X</a><br>".
-        "<h2>".$row["post"]."</h2>";
+        "<h1>".$row["usersuid"]."</h1>".
+        "<a href='../Application/deletepost.php?id=".$row['postID']."'>  X</a>
+        <br>".
+        "<h2>".$row['post']."</h2>"."<br>".
+        "<h2>".$row['content']."</h2>";
 
         ?><form action="../Application/comment.php" method="post" class="postbox">
         <textarea type="text" name="content" id="content" rows="2" cols="64" style="" placeholder="Comment"></textarea>
         <input type="hidden" name="uid" value="<?php echo $row["userID"]; ?>">
+        <input type="hidden" name="postID" value="<?php echo $row["postID"]; ?>">
         <br>
         <button type="submit" name="submit">Comment</button>
     </form><?php
